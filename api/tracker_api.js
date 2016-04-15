5ab4dd2b9a4f1a4acbca74592750e513f241526e
CryWolf <cry.wolf@centrum.cz>
2016-04-15 22:01:51 +0200

    Redis, log, nested querystring parsing

--- api/tracker_api.js
+++ api/tracker_api.js
@@ -1,11 +1,33 @@
 'use strict';
 
 const ApiController = require('./api_controller');
+const qs = require('qs'); // querystring parser that supports nesting and arrays
+const Q = require('q');
+const log = require('../models/log').module('Tracker Api');
 
 module.exports = class TrackerApi extends ApiController {
+
+  /**
+    @param {ctx} object Koa context
+    @returns promise
+  */
   track (ctx) {
     const query = ctx.query;
     ctx.body = `Hello World at ${new Date()}`;
-    ctx.body += ` | query: ${JSON.stringify(query)}`;
+    ctx.body += ` | querystring: ${JSON.stringify(qs.parse(ctx.querystring))}`;
+
+    const Redis = require('ioredis');
+    const redis = new Redis();
+
+    let result;
+    if (query.count) {
+      const count = parseInt(query.count, 10);
+      result = redis.incrby('count', count).catch(err => {
+        log.info(err, 'Error incrementing value in Redis');
+      });
+    } else {
+      result = new Q();
+    }
+    return result;
   }
 };
---
+++ models/log/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const winston = require('winston');
+
+module.exports = winston;
+
+module.exports.module = (moduleName, logger) => { // eslint-disable-line arrow-body-style
+
+  return {
+
+    name: `[${moduleName.toUpperCase()}]`,
+    logger: (logger || winston),
+
+    error (err, metaData) {
+      metaData = this._prepareMetaData(metaData);
+      this.logger.error(err, metaData);
+    },
+
+    warn (err, metaData) {
+      metaData = this._prepareMetaData(metaData);
+      this.logger.warn(err, metaData);
+    },
+
+    info (err, metaData) {
+      metaData = this._prepareMetaData(metaData);
+      this.logger.info(err, metaData);
+    },
+
+    _prepareMetaData (metaData) {
+      metaData = (metaData || {});
+      if (typeof metaData === 'string') {
+        metaData = { description: metaData };
+      }
+      metaData.module = this.name;
+      return metaData;
+    }
+
+  };
+
+};
